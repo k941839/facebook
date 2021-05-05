4ec16c69f31a28808af0e303cc665fafd506b70d
k941839 <83365757+k941839@users.noreply.github.com>
2021-05-05 12:26:09 -0400

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,19 @@
+import requests
+username = (input("State the Username to search: \n"))
+print ("=" * 60)
+print ("Searching Username: " + username)
+print ("=" * 60)
+
+url = 'https://www.facebook.com/' + username
+r = requests.get(url)
+if r.status_code == 200:
+    print ("=" * 60)
+    print ("\n")
+    print ("Profile found at: " + url)
+    print ("\n")
+    print ("=" * 60)
+if r.status_code == 404:
+    print ("Profile not found!")
+
+if r.status_code == 403:
+    print ("Server has blocked the connection!")
